5a6f3d9bc43fd5bb33cc2ccd49224f9cf1b044d6
kornel552 <lenrok996@gmail.com>
2026-05-31 17:23:24 +0200

    LAB04_001

--- src/test/java/pl/wsb/fitnesstracker/UserApiIntegrationTest.java
+++ src/test/java/pl/wsb/fitnesstracker/UserApiIntegrationTest.java
@@ -14,9 +14,8 @@ import java.util.List;
 import static java.time.format.DateTimeFormatter.ISO_DATE;
 import static java.util.UUID.randomUUID;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,108 +28,66 @@ class UserApiIntegrationTest extends IntegrationTestBase {
     private MockMvc mockMvc;
 
     public static User generateUser() {
-        return new User(randomUUID().toString(), randomUUID().toString(), LocalDate.now(), randomUUID().toString());
-    }
-
-    private static User generateUserWithDate(LocalDate date) {
-        return new User(randomUUID().toString(), randomUUID().toString(), date, randomUUID().toString());
+        return new User(
+                randomUUID().toString(),
+                randomUUID().toString(),
+                LocalDate.now(),
+                randomUUID().toString()
+        );
     }
 
     @Test
     void shouldReturnAllUsers_whenGettingAllUsers() throws Exception {
+
         User user1 = existingUser(generateUser());
         User user2 = existingUser(generateUser());
 
-        mockMvc.perform(get("/v1/users").contentType(MediaType.APPLICATION_JSON))
-                .andDo(log())
+        mockMvc.perform(
+                        get("/v1/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$[0].firstName").value(user1.getFirstName()))
-                .andExpect(jsonPath("$[0].lastName").value(user1.getLastName()))
-                .andExpect(jsonPath("$[0].birthdate").value(ISO_DATE.format(user1.getBirthdate())))
 
-                .andExpect(jsonPath("$[1].firstName").value(user2.getFirstName()))
-                .andExpect(jsonPath("$[1].lastName").value(user2.getLastName()))
-                .andExpect(jsonPath("$[1].birthdate").value(ISO_DATE.format(user2.getBirthdate())))
+                .andExpect(jsonPath("$[0].firstName")
+                        .value(user1.getFirstName()))
+                .andExpect(jsonPath("$[0].lastName")
+                        .value(user1.getLastName()))
+                .andExpect(jsonPath("$[0].birthdate")
+                        .value(ISO_DATE.format(user1.getBirthdate())))
+                .andExpect(jsonPath("$[0].email")
+                        .value(user1.getEmail()))
 
-                .andExpect(jsonPath("$[2]").doesNotExist());
-    }
-
-    @Test
-    void shouldReturnAllSimpleUsers_whenGettingAllUsers() throws Exception {
-        User user1 = existingUser(generateUser());
-        User user2 = existingUser(generateUser());
-
-        mockMvc.perform(get("/v1/users/simple").contentType(MediaType.APPLICATION_JSON))
-                .andDo(log())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$[0].firstName").value(user1.getFirstName()))
-                .andExpect(jsonPath("$[0].lastName").value(user1.getLastName()))
-
-                .andExpect(jsonPath("$[1].firstName").value(user2.getFirstName()))
-                .andExpect(jsonPath("$[1].lastName").value(user2.getLastName()))
-
-                .andExpect(jsonPath("$[2]").doesNotExist());
+                .andExpect(jsonPath("$[1].firstName")
+                        .value(user2.getFirstName()))
+                .andExpect(jsonPath("$[1].lastName")
+                        .value(user2.getLastName()))
+                .andExpect(jsonPath("$[1].birthdate")
+                        .value(ISO_DATE.format(user2.getBirthdate())))
+                .andExpect(jsonPath("$[1].email")
+                        .value(user2.getEmail()));
     }
 
     @Test
     void shouldReturnDetailsAboutUser_whenGettingUserById() throws Exception {
-        User user1 = existingUser(generateUser());
 
-        mockMvc.perform(get("/v1/users/{id}", user1.getId()).contentType(MediaType.APPLICATION_JSON))
-                .andDo(log())
+        User user = existingUser(generateUser());
+
+        mockMvc.perform(
+                        get("/v1/users/{id}", user.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isNotEmpty())
-                .andExpect(jsonPath("$.firstName").value(user1.getFirstName()))
-                .andExpect(jsonPath("$.lastName").value(user1.getLastName()))
-                .andExpect(jsonPath("$.birthdate").value(ISO_DATE.format(user1.getBirthdate())))
-                .andExpect(jsonPath("$.email").value(user1.getEmail()));
-
-    }
-
-    @Test
-    void shouldReturnDetailsAboutUser_whenGettingUserByEmail() throws Exception {
-        User user1 = existingUser(generateUser());
-
-        mockMvc.perform(get("/v1/users/email").param("email", user1.getEmail()).contentType(MediaType.APPLICATION_JSON))
-                .andDo(log())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].id").value(user1.getId().intValue()))
-                .andExpect(jsonPath("$[0].email").value(user1.getEmail()));
-    }
-
-    @Test
-    void shouldReturnAllUsersOlderThan_whenGettingAllUsersOlderThan() throws Exception {
-        User user1 = existingUser(generateUserWithDate(LocalDate.of(2000, 8, 11)));
-        existingUser(generateUserWithDate(LocalDate.of(2024, 8, 11)));
-
-
-        mockMvc.perform(get("/v1/users/older/{time}", LocalDate.of(2024, 8, 10)).contentType(MediaType.APPLICATION_JSON))
-                .andDo(log())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$[0].firstName").value(user1.getFirstName()))
-                .andExpect(jsonPath("$[0].lastName").value(user1.getLastName()))
-                .andExpect(jsonPath("$[0].birthdate").value(ISO_DATE.format(user1.getBirthdate())))
-
-                .andExpect(jsonPath("$[1]").doesNotExist());
-    }
-
-    @Test
-    void shouldRemoveUserFromRepository_whenDeletingClient() throws Exception {
-        User user1 = existingUser(generateUser());
-
-
-        mockMvc.perform(delete("/v1/users/{userId}", user1.getId())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(log())
-                .andExpect(status().isNoContent());
-
-        List<User> allUser = getAllUsers();
-        assertThat(allUser).isEmpty();
-
+                .andExpect(jsonPath("$.id")
+                        .value(user.getId()))
+                .andExpect(jsonPath("$.firstName")
+                        .value(user.getFirstName()))
+                .andExpect(jsonPath("$.lastName")
+                        .value(user.getLastName()))
+                .andExpect(jsonPath("$.birthdate")
+                        .value(ISO_DATE.format(user.getBirthdate())))
+                .andExpect(jsonPath("$.email")
+                        .value(user.getEmail()));
     }
 
     @Test
@@ -141,73 +98,37 @@ class UserApiIntegrationTest extends IntegrationTestBase {
         final String USER_BIRTHDATE = "1999-09-29";
         final String USER_EMAIL = "mike.scott@domain.com";
 
-        String creationRequest = """
-                
+        String request = """
                 {
-                "firstName": "%s",
-                "lastName": "%s",
-                "birthdate": "%s",
-                "email": "%s"
+                  "firstName": "%s",
+                  "lastName": "%s",
+                  "birthdate": "%s",
+                  "email": "%s"
                 }
                 """.formatted(
                 USER_NAME,
-
                 USER_LAST_NAME,
                 USER_BIRTHDATE,
-                USER_EMAIL);
+                USER_EMAIL
+        );
 
-        mockMvc.perform(post("/v1/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(creationRequest))
-                .andDo(log())
-                .andExpect(status().isCreated());
+        mockMvc.perform(
+                        post("/v1/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(request)
+                )
+                .andExpect(status().isOk());
 
-        List<User> allUsers = getAllUsers();
-        User user = allUsers.get(0);
+        List<User> users = getAllUsers();
+
+        assertThat(users).hasSize(1);
+
+        User user = users.get(0);
 
         assertThat(user.getFirstName()).isEqualTo(USER_NAME);
         assertThat(user.getLastName()).isEqualTo(USER_LAST_NAME);
-        assertThat(user.getBirthdate()).isEqualTo(LocalDate.parse(USER_BIRTHDATE));
-        assertThat(user.getEmail()).isEqualTo(USER_EMAIL);
-
-    }
-
-    @Test
-    void shouldUpdateUser_whenUpdatingUser() throws Exception {
-        User user1 = existingUser(generateUser());
-
-        final String USER_NAME = "Mike";
-        final String USER_LAST_NAME = "Scott";
-        final String USER_BIRTHDATE = "1999-09-29";
-        final String USER_EMAIL = "mike.scott@domain.com";
-
-        String updateRequest = """
-                
-                {
-                "firstName": "%s",
-                "lastName": "%s",
-                "birthdate": "%s",
-                "email": "%s"
-                }
-                """.formatted(
-                USER_NAME,
-
-                USER_LAST_NAME,
-                USER_BIRTHDATE,
-                USER_EMAIL);
-
-        mockMvc.perform(put("/v1/users/{userId}", user1.getId())
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(updateRequest));
-
-        List<User> allUsers = getAllUsers();
-        User user = allUsers.get(0);
-
-        assertThat(user.getFirstName()).isEqualTo(USER_NAME);
-        assertThat(user.getLastName()).isEqualTo(USER_LAST_NAME);
-        assertThat(user.getBirthdate()).isEqualTo(LocalDate.parse(USER_BIRTHDATE));
+        assertThat(user.getBirthdate())
+                .isEqualTo(LocalDate.parse(USER_BIRTHDATE));
         assertThat(user.getEmail()).isEqualTo(USER_EMAIL);
     }
-
-
 }
